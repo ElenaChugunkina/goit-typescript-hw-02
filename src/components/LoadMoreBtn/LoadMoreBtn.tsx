1d0a1d1fd96a583637f963bd9c42f384856fd029
ElenaChugunkina <emudrak.mudrak15@gmail.com>
2024-08-05 19:16:33 +0300

    change

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,13 +1,4 @@
-// import css from './LoadMoreBtn.module.css'
-// const LoadMoreBtn = ({onClick}) => {
-//     return (
-//         <div className={css.container}>
-//             <button type="submit" className={css.btnLoad} onClick={onClick} >Load more</button>
-//         </div>
-//     )
-// }
 
-// export default LoadMoreBtn;
 
 import React from 'react';
 import css from './LoadMoreBtn.module.css';
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,58 +1,19 @@
 
-// import css from './SearchBar.module.css'
-
-// import React from 'react';
-
-// import toast, { Toaster } from 'react-hot-toast';
-
-// const SearchBar = ({ onSubmit }) => {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const form = event.target;
-//     const query = form.elements.topic.value.trim();
-
-//     if (query === '') {
-//       toast.error('The field must not be empty.');
-//       return;
-//     }
-
-//     onSubmit(query);
-//     form.reset();
-//   };
-
-//   return (
-//     <header>
-//       <form className={css.container} onSubmit={handleSubmit}>
-//         <input className={css.input}
-//           type="text"
-//           autoComplete="off"
-//           autoFocus
-//           placeholder="Search images and photos"
-//           name="topic"
-//         />
-//         <button className={css.btn} type="submit">Search</button>
-//         <Toaster />
-//       </form>
-//     </header>
-//   );
-// };
-
-// export default SearchBar;
 
 import React from 'react';
 
 import css from './SearchBar.module.css';
 
+
 import toast, { Toaster } from 'react-hot-toast';
 
-// Визначення типу пропсів
 interface SearchBarProps {
   onSubmit: (query: string) => void;
 }
 
-// Компонент SearchBar
+
 const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
-  // Функція обробки сабміту форми
+  
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.currentTarget;
